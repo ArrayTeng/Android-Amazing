2d49693a9405193dbd5762b1ec954c0f8d4c6f72
tengfei <tengfei599100@163.com>
2019-01-21 14:46:42 +0800

    MaterialDesign

---
+++ 05_MaterialDesign/app/src/main/java/com/example/material/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.material;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author tengfei
+ * date 2019/1/21 2:45 PM
+ * email tengfeigo@outlook.com
+ * description
+*/
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
