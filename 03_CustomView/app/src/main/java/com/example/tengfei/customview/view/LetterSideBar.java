8f3f98c325039d3e967e20262822bf3f1c8a4596
tengfei <tengfei599100@163.com>
2019-01-22 23:09:31 +0800

    update

---
+++ 03_CustomView/app/src/main/java/com/example/tengfei/customview/view/LetterSideBar.java
@@ -0,0 +1,10 @@
+package com.example.tengfei.customview.view;
+
+/**
+ * @author tengfei
+ * date 2019/1/22 11:08 PM
+ * email tengfeigo@outlook.com
+ * description 自定义字母索引列表
+ */
+public class LetterSideBar {
+}
