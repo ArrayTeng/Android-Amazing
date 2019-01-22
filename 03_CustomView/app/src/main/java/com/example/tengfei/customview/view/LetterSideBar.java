c25a310ba6a94869c88d56b6c6e12f7fdd7774dc
tengfei <tengfei599100@163.com>
2019-01-23 00:56:37 +0800

    字母索引列表

--- 03_CustomView/app/src/main/java/com/example/tengfei/customview/view/LetterSideBar.java
+++ 03_CustomView/app/src/main/java/com/example/tengfei/customview/view/LetterSideBar.java
@@ -1,10 +1,52 @@
 package com.example.tengfei.customview.view;
 
+import android.content.Context;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.View;
+
 /**
  * @author tengfei
  * date 2019/1/22 11:08 PM
  * email tengfeigo@outlook.com
  * description 自定义字母索引列表
  */
-public class LetterSideBar {
+public class LetterSideBar extends View {
+
+    private Paint mPaint;
+
+    private int mColor;
+    private int mTextSize;
+
+    public LetterSideBar(Context context) {
+        this(context, null);
+    }
+
+    public LetterSideBar(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public LetterSideBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        //动态调整
+        mPaint.setColor(mColor);
+        mPaint.setTextSize(sp2px(20));
+    }
+
+    private float sp2px(float sp) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp, getResources().getDisplayMetrics());
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        //返回测量的文本的宽度
+        int textWidth = (int) mPaint.measureText("A");
+        int width = getPaddingLeft() + getPaddingRight() + textWidth;
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+        setMeasuredDimension(width, height);
+    }
 }
