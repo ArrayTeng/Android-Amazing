17178ec7421f1a22d53e1485a3844cc3ed6b2db8
tengfei <tengfei599100@163.com>
2019-02-15 15:42:24 +0800

    update

--- Example/app/src/main/java/com/example/tengfei/activity/CityIndexListActivity.java
+++ Example/app/src/main/java/com/example/tengfei/activity/CityIndexListActivity.java
@@ -3,18 +3,11 @@ package com.example.tengfei.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.view.textclassifier.TextClassification;
 
 import com.example.tengfei.BaseActivity;
 import com.example.tengfei.R;
 
-import java.io.IOException;
-
-import okhttp3.Call;
-import okhttp3.Callback;
 import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 /**
  * @author tengfei
@@ -32,18 +25,7 @@ public class CityIndexListActivity extends BaseActivity {
     @Override
     public void initOperation(@Nullable Bundle savedInstanceState) {
         OkHttpClient okHttpClient = new OkHttpClient();
-        Request request = new Request.Builder();
-        okHttpClient.newCall().enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
 
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-
-            }
-        });
         
     }
 
