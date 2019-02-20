561145555814ef1c9355e364ea9b2a9f74c56df2
tengfei <tengfei599100@163.com>
2019-02-20 14:47:09 +0800

    update

--- Example/app/src/main/java/com/example/tengfei/activity/CityIndexListActivity.java
+++ Example/app/src/main/java/com/example/tengfei/activity/CityIndexListActivity.java
@@ -3,9 +3,25 @@ package com.example.tengfei.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Base64;
+import android.util.JsonToken;
+import android.util.Log;
+import android.view.View;
+
 import com.example.tengfei.BaseActivity;
 import com.example.tengfei.R;
+import com.google.gson.Gson;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 
 /**
@@ -24,6 +40,42 @@ public class CityIndexListActivity extends BaseActivity {
     @Override
     public void initOperation(@Nullable Bundle savedInstanceState) {
 
+
+        CreateAuthorization createAuthorization = new CreateAuthorization();
+
+        createAuthorization.note = GithubConfig.NOTE;
+        createAuthorization.client_id = GithubConfig.CLIENT_ID;
+        createAuthorization.client_secret = GithubConfig.CLIENT_SECRET;
+        createAuthorization.scopes = GithubConfig.SCOPES;
+        // https://developer.github.com/v3/oauth/#non-web-application-flow
+        String userCredentials = "TengfeiGo" + ":" + "TengFei1995";
+        Gson gson = new Gson();
+        String basicAuth =
+                "Basic " + new String(Base64.encode(userCredentials.getBytes(), Base64.DEFAULT));
+        findViewById(R.id.bt).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                RequestBody requestBody = RequestBody.create(MediaType.parse("application/json"), gson.toJson(createAuthorization));
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request request = new Request.Builder()
+                        .url("https://api.github.com/user?access_token=a5ce084e4f67f90f82dd7e6a437e524cf946e78e")
+//                        .post(requestBody)
+                        .addHeader("Authorization", basicAuth.trim())
+                        .build();
+                Call call = okHttpClient.newCall(request);
+                call.enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        Log.i("tmd", "failure" + e.getMessage());
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        Log.i("tmd", response.body().string());
+                    }
+                });
+            }
+        });
 
     }
 
---
+++ Example/app/src/main/java/com/example/tengfei/activity/GithubConfig.java
@@ -0,0 +1,18 @@
+package com.example.tengfei.activity;
+
+/**
+ * Created by mingjun on 16/7/27.
+ */
+public class GithubConfig {
+
+    // client id/secret
+    public static final String CLIENT_ID = "6283ea85a19154300df8";
+    public static final String CLIENT_SECRET = "ddbb7d80fc64cac856adac58eebc6e2dbb7c5e92";
+
+    // scopes
+    public static final String[] SCOPES = {"user", "repo", "notifications", "gist", "admin:org"};
+
+    public static final String NOTE = "GithubApp";
+
+    public static final String AUTHOR_NAME = "mingjunli";
+}
