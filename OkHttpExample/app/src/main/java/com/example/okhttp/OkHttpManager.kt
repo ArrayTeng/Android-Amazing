f57e987d5be514002ce90a661a80fbc00412efa2
tengfei <tengfei599100@163.com>
2019-03-04 16:22:31 +0800

    okhttp

---
+++ OkHttpExample/app/src/main/java/com/example/okhttp/OkHttpManager.kt
@@ -0,0 +1,11 @@
+package com.example.okhttp
+
+/**
+ * @author tengfei
+ * date 2019/3/4 3:08 PM
+ * email tengfeigo@outlook.com
+ * description OkHttp管理类，同步和异步方法封装
+ */
+class OkHttpManager private constructor(){
+
+}
