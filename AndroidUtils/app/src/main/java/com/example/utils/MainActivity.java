e1425d28bdd121846817fda6eb495b31d5118b55
tengfei <tengfei599100@163.com>
2019-04-01 15:39:53 +0800

    update

--- AndroidUtils/app/src/main/java/com/example/utils/MainActivity.java
+++ AndroidUtils/app/src/main/java/com/example/utils/MainActivity.java
@@ -3,6 +3,14 @@ package com.example.utils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 /**
  * @author tengfei
  * date 2019/3/21 9:26 AM
@@ -16,6 +24,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .build();
+        Request request = new Request.Builder().build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+            }
+        });
 
     }
 
