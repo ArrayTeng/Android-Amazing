a0d4126317bdc55321a5f03e126f5d153350fd52
tengfei <tengfei599100@163.com>
2019-04-03 21:18:52 +0800

    update

--- AndroidUtils/app/src/main/java/com/example/utils/MainActivity.java
+++ AndroidUtils/app/src/main/java/com/example/utils/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.utils;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import java.io.IOException;
 
@@ -23,22 +24,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        OkHttpClient okHttpClient = new OkHttpClient.Builder()
-                .build();
-        Request request = new Request.Builder().build();
-        Call call = okHttpClient.newCall(request);
-        call.enqueue(new Callback() {
+        findViewById(R.id.bt).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onFailure(Call call, IOException e) {
+            public void onClick(View v) {
+                OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                        .build();
+                Request request = new Request.Builder()
+                        .url("https://www.baidu.com/")
+                        .build();
+                Call call = okHttpClient.newCall(request);
+                //调用 RealCall 的 enqueue 方法
+                call.enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
 
-            }
+                    }
 
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
 
+                    }
+                });
             }
         });
+
 
     }
 
