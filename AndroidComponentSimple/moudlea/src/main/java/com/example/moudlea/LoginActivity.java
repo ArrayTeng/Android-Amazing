456f58d8b4144c7e4c67a70796ab3203771edf33
tengfei <tengfei599100@163.com>
2019-04-05 14:39:07 +0800

    组件化架构

---
+++ AndroidComponentSimple/moudlea/src/main/java/com/example/moudlea/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.example.moudlea;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.component.R;
+
+/**
+ * @author tengfei
+ * date 2019/4/5 2:28 PM
+ * email tengfeigo@outlook.com
+ * description
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
