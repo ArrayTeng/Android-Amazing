356c6fa26bc9b88b41e3221c35045fb0635c0b57
tengfei <tengfei599100@163.com>
2019-06-12 22:24:27 +0800

    update

---
+++ JetPackFramwork/app/src/main/java/com/example/tengfei/jetpack/LiveDataTimerViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.tengfei.jetpack
+
+import android.arch.lifecycle.ViewModel
+
+/**
+ * @author tengfei
+ * date 2019/6/12 10:21 PM
+ * email tengfeigo@outlook.com
+ * description  思考 1 ViewModel的作用，观看官方视频然后回答 2 liveData的作用以及应用范围
+ */
+class LiveDataTimerViewModel : ViewModel() {
+
+
+}
