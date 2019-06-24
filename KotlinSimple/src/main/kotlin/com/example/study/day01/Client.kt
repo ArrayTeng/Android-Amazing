828ba4f5ffb12d801fe2553602d875a8fd6b376a
tengfei <tengfei599100@163.com>
2019-06-24 20:32:10 +0800

    upeta

--- KotlinSimple/src/main/kotlin/com/example/study/day01/Client.kt
+++ KotlinSimple/src/main/kotlin/com/example/study/day01/Client.kt
@@ -1,6 +1,7 @@
 package com.example.study.day01
 
 import com.example.study.day01.Color.*
+import java.util.*
 
 /**
  * @author tengfei
@@ -35,13 +36,70 @@ fun main(args: Array<String>) {
 
     println("${eval(Num(1))}")
 
-    println("if 和 when 都可以使用代码块作为分支体，这种情况下代码块中的最后一个表达式就是结果 ${evalWithLogging(Sum(Num(2),Num(3)))}")
+    println("if 和 when 都可以使用代码块作为分支体，这种情况下代码块中的最后一个表达式就是结果 ${evalWithLogging(Sum(Num(2), Num(3)))}")
+
+    for (i in 1..100) {
+        print(fizzBuzzFirstToDown(i))
+    }
+    println()
+    for (i in 100 downTo 1 step 2) {
+        print(fizzBuzzFirstToDown(i) + "  ")
+    }
+    println()
+    for (i in 0 until 100) {
+        print(fizzBuzzFirstToDown(i))
+    }
+    println()
+    println()
+    println()
+    val binaryReps = TreeMap<Char, String>()
+    for (c in 'A'..'F') {
+        binaryReps[c] = Integer.toBinaryString(c.toInt())
+    }
+    for ((key, value) in binaryReps) {
+        println("$key $value")
+    }
+
+
+    println()
+    println()
+    println()
 
     /**
-     * 迭代事物 while 和 for 循环
+     * 迭代集合时使用下标
      */
+    val list = arrayListOf("10", "22", "145", "31")
+    for ((index, element) in list.withIndex()) {
+        println("$index $element")
+    }
 
+    /**
+     * 使用"in"检查区间和集合的成员
+     */
+    println("${isLetter('f')}  ${isNotDigit(4)}")
+
+    /**
+     * 用 in 检查作为 when 分支
+     */
+    println(recognize('f'))
 
 }
 
+fun recognize(c: Char) = when (c) {
+    in '0'..'9' -> "lt is letter"
+    in 'a'..'z', in 'A'..'Z' -> "lt is buzzer"
+    else -> "I don't know"
+}
+
+fun isLetter(c: Char) = c in 'a'..'z' || c in 'A'..'Z'
+
+fun isNotDigit(i: Int) = i !in 0..10
+
 fun max(a: Int, b: Int) = if (a > b) a else b
+
+fun fizzBuzzFirstToDown(i: Int): String = when {
+    i % 15 == 0 -> "fizzBuzz"
+    i % 3 == 0 -> "fizz"
+    i % 5 == 0 -> "Buzz"
+    else -> i.toString()
+}
