ebfc44c5b8b2a04dd53ee4cb867af1f12bcdf089
tengfei <tengfeigo@outlook.com>
2019-09-15 16:52:51 +0800

    完成了主界面

---
+++ flutter_wechat_find/lib/ContentPage.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_wechat_find/CustomAppBar.dart';
+
+class ContentPage extends StatefulWidget {
+  final ValueChanged<int> valueChanged;
+  final ContentPageController contentPageController;
+
+  const ContentPage({Key key, this.valueChanged, this.contentPageController})
+      : super(key: key);
+
+  @override
+  _ContentPageState createState() {
+    return _ContentPageState();
+  }
+}
+
+class _ContentPageState extends State<ContentPage> {
+  List<Color> _colors = [Colors.blue, Colors.grey, Colors.red, Colors.black];
+  PageController _pageController = PageController(viewportFraction: 0.8);
+
+  @override
+  void initState() {
+    super.initState();
+    _statusBar();
+    if (widget.contentPageController != null) {
+      widget.contentPageController.pageController = _pageController;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        CustomAppBar(),
+        //Expanded用来撑开布局
+        Expanded(
+            child: PageView(
+          onPageChanged: widget.valueChanged,
+          controller: _pageController,
+          children: <Widget>[_wrapItem(0), _wrapItem(1), _wrapItem(2)],
+        ))
+      ],
+    );
+  }
+
+  _wrapItem(int index) {
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: Container(
+        decoration: BoxDecoration(color: _colors[index]),
+      ),
+    );
+  }
+
+  //实现沉浸式状态栏
+  _statusBar() {
+    SystemUiOverlayStyle systemUiOverlayStyle = SystemUiOverlayStyle(
+      systemNavigationBarColor: Color(0xFF000000),
+      systemNavigationBarDividerColor: null,
+      statusBarColor: Colors.transparent,
+      systemNavigationBarIconBrightness: Brightness.light,
+      statusBarIconBrightness: Brightness.dark,
+      statusBarBrightness: Brightness.light,
+    );
+
+    SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
+  }
+}
+
+class ContentPageController {
+  PageController pageController;
+
+  void jumpToPage(int indexPage) {
+    pageController?.jumpToPage(indexPage);
+  }
+}
