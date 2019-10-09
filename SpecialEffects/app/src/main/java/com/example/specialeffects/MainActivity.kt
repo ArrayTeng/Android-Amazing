02b8a2d245eb34982ecffa5ec43c405495e8a794
tengfei <tengfeigo@outlook.com>
2019-10-09 17:09:55 +0800

    update

--- SpecialEffects/app/src/main/java/com/example/specialeffects/MainActivity.kt
+++ SpecialEffects/app/src/main/java/com/example/specialeffects/MainActivity.kt
@@ -1,22 +1,31 @@
 package com.example.specialeffects
 
+import android.animation.Keyframe
 import android.animation.ObjectAnimator
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.PropertyValuesHolder
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.specialeffects.utils.px2dp
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        ObjectAnimator.ofFloat(cameraView,"bottomFlip",  150F)
-            .setDuration(3000)
-            .start()
+        val keyframe0 = Keyframe.ofFloat(0F, px2dp(50F))
+        val keyframe1 = Keyframe.ofFloat(0.5F,  px2dp(100F))
+        val keyframe2 = Keyframe.ofFloat(1F, px2dp(50F))
+
+        val propertyValuesHolder =
+            PropertyValuesHolder.ofKeyframe("radious", keyframe0, keyframe1, keyframe2)
+
+        val animator = ObjectAnimator.ofPropertyValuesHolder(circleView, propertyValuesHolder)
+        animator.startDelay = 3000
+        animator.duration = 5000
+        animator.start()
 
 
     }
