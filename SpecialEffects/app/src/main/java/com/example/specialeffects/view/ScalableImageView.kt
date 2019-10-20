2236133c513bb940adc61f85d32610707a28f71d
tengfei <tengfeigo@outlook.com>
2019-10-20 21:26:51 +0800

    update

--- SpecialEffects/app/src/main/java/com/example/specialeffects/view/ScalableImageView.kt
+++ SpecialEffects/app/src/main/java/com/example/specialeffects/view/ScalableImageView.kt
@@ -10,6 +10,7 @@ import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.GestureDetector
 import android.view.MotionEvent
+import android.view.ScaleGestureDetector
 import android.view.View
 import android.widget.OverScroller
 import androidx.core.view.GestureDetectorCompat
@@ -26,8 +27,7 @@ import kotlin.math.min
  */
 class ScalableImageView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
-) : View(context, attrs, defStyleAttr), GestureDetector.OnGestureListener,
-    GestureDetector.OnDoubleTapListener, Runnable {
+) : View(context, attrs, defStyleAttr) {
 
     private var offsetX = 0f
     private var offsetY = 0f
@@ -42,11 +42,23 @@ class ScalableImageView @JvmOverloads constructor(
 
     private val bitmap: Bitmap
 
+    /**
+     * 手势检测监听器
+     */
     private val gestureDetector: GestureDetectorCompat
+
+    /**
+     * 手势缩放监听器
+     */
+    private val scaleGestureDetector: ScaleGestureDetector
+
+    private val henOnScaleGestureListener = HenOnScaleGestureListener()
+    private val henGestureListener = HenGestureListener()
+    private val henOnFlingRunnable = HenOnFlingRunnable()
 
     private val overScroller: OverScroller
 
-    private var scaleFraction: Float = 0F
+    private var currentScale: Float = 0F
         get() = field
         set(value) {
             field = value
@@ -55,102 +67,30 @@ class ScalableImageView @JvmOverloads constructor(
 
     private val scaleObjectAnimator: ObjectAnimator by lazy {
         //在View里定义了 scaleFraction 属性通过属性动画来操作这个动画的 scaleFraction 的值
-        ObjectAnimator.ofFloat(this@ScalableImageView, "scaleFraction", 0F, 1F)
+        ObjectAnimator.ofFloat(
+            this@ScalableImageView, "currentScale",
+            smallScale, bigScale
+        )
+
     }
 
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
     init {
         bitmap = getAvatar(dp2px(100F).toInt())
-        gestureDetector = GestureDetectorCompat(context, this)
+        gestureDetector = GestureDetectorCompat(context, henGestureListener)
+        scaleGestureDetector = ScaleGestureDetector(context, henOnScaleGestureListener)
         overScroller = OverScroller(context)
     }
 
-    /**
-     * 如果控件设置了双击，那么 onSingleTapUp 失效并且在 onDoubleTap 情况下单击由 onSingleTapConfirmed 处理
-     */
-    override fun onDoubleTap(e: MotionEvent): Boolean {
-        big = !big
-        if (big) {
-            offsetX = (e.x - width / 2) - (e.x - width / 2) * (bigScale / smallScale)
-            offsetY = (e.y - height / 2) - (e.y - height / 2) * (bigScale / smallScale)
-            scaleObjectAnimator.start()
-        } else {
-            scaleObjectAnimator.reverse()
-        }
-        invalidate()
-        return false
-    }
-
-    override fun onDoubleTapEvent(e: MotionEvent?): Boolean = false
-
-    override fun onSingleTapConfirmed(e: MotionEvent?): Boolean = false
-
-    override fun onShowPress(e: MotionEvent?) = Unit
-
-    override fun onSingleTapUp(e: MotionEvent?): Boolean = false
-
-    //对应于 down 事件
-    override fun onDown(e: MotionEvent?): Boolean = true
-
-    //惯性滑动
-    override fun onFling(
-        e1: MotionEvent?,
-        e2: MotionEvent?,
-        velocityX: Float,
-        velocityY: Float
-    ): Boolean {
-        if (big) {
-            //左右滑动的最大偏移量
-            val maxOffsetX: Int = ((bitmap.width * bigScale - width) / 2).toInt()
-            //上下滑动的最大偏移量
-            val maxOffsetY: Int = ((bitmap.height * bigScale - height) / 2).toInt()
-            overScroller.fling(
-                offsetX.toInt(),
-                offsetY.toInt(),
-                velocityX.toInt(),
-                velocityY.toInt(),
-                -maxOffsetX,
-                maxOffsetX,
-                -maxOffsetY,
-                maxOffsetY
-            )
-
-            postOnAnimation(this)
-        }
-
-        return false
-    }
-
-    override fun onScroll(
-        downEvent: MotionEvent?,
-        currentEvent: MotionEvent?,
-        distanceX: Float,
-        distanceY: Float
-    ): Boolean {
-        if (big) {
-            //左右滑动的最大偏移量
-            val maxOffsetX = (bitmap.width * bigScale - width) / 2
-            //上下滑动的最大偏移量
-            val maxOffsetY = (bitmap.height * bigScale - height) / 2
-            offsetX -= distanceX
-            offsetX = min(offsetX, maxOffsetX)
-            offsetX = max(offsetX, -maxOffsetX)
-
-            offsetY -= distanceY
-            offsetY = min(offsetY, maxOffsetY)
-            offsetY = max(offsetY, -maxOffsetY)
-
-            invalidate()
-        }
-        return false
-    }
-
-    override fun onLongPress(e: MotionEvent?) = Unit
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(event: MotionEvent?): Boolean {
-        return gestureDetector.onTouchEvent(event)
+        var result = scaleGestureDetector.onTouchEvent(event)
+        if (!scaleGestureDetector.isInProgress){
+            result =  gestureDetector.onTouchEvent(event)
+        }
+        return result
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -167,23 +107,15 @@ class ScalableImageView @JvmOverloads constructor(
             smallScale = (height / bitmap.height).toFloat()
             bigScale = (width / bitmap.width).toFloat()
         }
+        this.currentScale = smallScale
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+        val scaleFraction = (currentScale - smallScale) / (bigScale - smallScale)
         canvas.translate(offsetX * scaleFraction, offsetY * scaleFraction)
-        val scale = smallScale + (bigScale - smallScale) * scaleFraction
-        canvas.scale(scale, scale, (width / 2).toFloat(), (height / 2).toFloat())
+        canvas.scale(currentScale, currentScale, (width / 2).toFloat(), (height / 2).toFloat())
         canvas.drawBitmap(bitmap, originalOffsetX, originalOffsetY, paint)
-    }
-
-    override fun run() {
-        if (overScroller.computeScrollOffset()) {
-            offsetX = overScroller.currX.toFloat()
-            offsetY = overScroller.currY.toFloat()
-            invalidate()
-            postOnAnimation(this)
-        }
     }
 
 
@@ -195,6 +127,124 @@ class ScalableImageView @JvmOverloads constructor(
         options.inDensity = options.outWidth
         options.inTargetDensity = width
         return BitmapFactory.decodeResource(resources, R.drawable.avatar_rengwuxian, options)
+    }
+
+    private inner class HenOnScaleGestureListener : ScaleGestureDetector.OnScaleGestureListener {
+
+        var initializerScale: Float = 0F
+
+        override fun onScaleBegin(detector: ScaleGestureDetector?): Boolean {
+            initializerScale = currentScale
+            return true
+        }
+
+        override fun onScaleEnd(detector: ScaleGestureDetector?) {
+        }
+
+        override fun onScale(detector: ScaleGestureDetector?): Boolean {
+            currentScale = initializerScale * detector!!.scaleFactor
+            invalidate()
+            return false
+        }
+    }
+
+    private inner class HenOnFlingRunnable : Runnable {
+        override fun run() {
+            if (overScroller.computeScrollOffset()) {
+                offsetX = overScroller.currX.toFloat()
+                offsetY = overScroller.currY.toFloat()
+                invalidate()
+                postOnAnimation(this)
+            }
+        }
+    }
+
+    private inner class HenGestureListener : GestureDetector.OnGestureListener,
+        GestureDetector.OnDoubleTapListener {
+
+        /**
+         * 如果控件设置了双击，那么 onSingleTapUp 失效并且在 onDoubleTap 情况下单击由 onSingleTapConfirmed 处理
+         */
+        override fun onDoubleTap(e: MotionEvent): Boolean {
+            big = !big
+            if (big) {
+                //双击的点在原地，本质上就是在放大后做一个逆向的偏移
+                offsetX = (e.x - width / 2) - (e.x - width / 2) * (bigScale / smallScale)
+                offsetY = (e.y - height / 2) - (e.y - height / 2) * (bigScale / smallScale)
+                scaleObjectAnimator.start()
+            } else {
+                scaleObjectAnimator.reverse()
+            }
+            invalidate()
+            return false
+        }
+
+        override fun onDoubleTapEvent(e: MotionEvent?): Boolean = false
+
+        override fun onSingleTapConfirmed(e: MotionEvent?): Boolean = false
+
+        override fun onShowPress(e: MotionEvent?) = Unit
+
+        override fun onSingleTapUp(e: MotionEvent?): Boolean = false
+
+        //对应于 down 事件
+        override fun onDown(e: MotionEvent?): Boolean = true
+
+        //惯性滑动
+        override fun onFling(
+            e1: MotionEvent?,
+            e2: MotionEvent?,
+            velocityX: Float,
+            velocityY: Float
+        ): Boolean {
+            if (big) {
+                //左右滑动的最大偏移量
+                val maxOffsetX: Int = ((bitmap.width * bigScale - width) / 2).toInt()
+                //上下滑动的最大偏移量
+                val maxOffsetY: Int = ((bitmap.height * bigScale - height) / 2).toInt()
+                overScroller.fling(
+                    offsetX.toInt(),
+                    offsetY.toInt(),
+                    velocityX.toInt(),
+                    velocityY.toInt(),
+                    -maxOffsetX,
+                    maxOffsetX,
+                    -maxOffsetY,
+                    maxOffsetY
+                )
+
+                postOnAnimation(henOnFlingRunnable)
+            }
+
+            return false
+        }
+
+        override fun onScroll(
+            downEvent: MotionEvent?,
+            currentEvent: MotionEvent?,
+            distanceX: Float,
+            distanceY: Float
+        ): Boolean {
+            if (big) {
+                //左右滑动的最大偏移量
+                val maxOffsetX = (bitmap.width * bigScale - width) / 2
+                //上下滑动的最大偏移量
+                val maxOffsetY = (bitmap.height * bigScale - height) / 2
+                offsetX -= distanceX
+                offsetX = min(offsetX, maxOffsetX)
+                offsetX = max(offsetX, -maxOffsetX)
+
+                offsetY -= distanceY
+                offsetY = min(offsetY, maxOffsetY)
+                offsetY = max(offsetY, -maxOffsetY)
+
+                invalidate()
+            }
+            return false
+        }
+
+        override fun onLongPress(e: MotionEvent?) = Unit
+
     }
 }
 
