3c7af29c7345665261f77b1596e56221ccf465d3
tengfei <tengfeigo@outlook.com>
2019-11-05 22:04:03 +0800

    update

---
+++ JavaPoetDemo/poet/src/main/java/com/example/poet/IPoet.java
@@ -0,0 +1,10 @@
+package com.example.poet;
+
+/**
+ * @author tengfei
+ * date 2019-11-05 20:11
+ * email tengfeigo@outlook.com
+ * description
+ */
+public interface IPoet {
+}
---
+++ JavaPoetDemo/poet/src/main/java/com/example/poet/Presenter.java
@@ -0,0 +1,10 @@
+package com.example.poet;
+
+/**
+ * @author tengfei
+ * date 2019-11-05 20:14
+ * email tengfeigo@outlook.com
+ * description
+ */
+public class Presenter<String> {
+}
