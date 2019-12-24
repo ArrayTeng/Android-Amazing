21a8ac8aad8d5df6b18804d1f048faa94e627ed3
tengfei <arrayadapter.cn@gmail.com>
2019-12-25 00:52:52 +0800

    update

---
+++ AlgorithmDemo/src/main/java/com/example/algorithm/day02/LinkedList.java
@@ -0,0 +1,25 @@
+package com.example.algorithm.day02;
+
+/**
+ * @author tengfei
+ */
+public class LinkedList<E> {
+
+    private class Node {
+        public E data;
+        public Node next;
+
+        public Node(E data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        public Node(E data) {
+            this(data, null);
+        }
+
+        public Node() {
+            this(null, null);
+        }
+    }
+}
