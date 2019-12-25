47014ad4b1b98007af2b09c5f36551b702d3f275
tengfei <arrayadapter.cn@gmail.com>
2019-12-25 23:15:34 +0800

    链表B

--- AlgorithmDemo/src/main/java/com/example/algorithm/day02/LinkedList.java
+++ AlgorithmDemo/src/main/java/com/example/algorithm/day02/LinkedList.java
@@ -5,6 +5,10 @@ package com.example.algorithm.day02;
  */
 public class LinkedList<E> {
 
+    private Node dummyHeader;
+
+    private int size;
+
     private class Node {
         public E data;
         public Node next;
@@ -22,4 +26,45 @@ public class LinkedList<E> {
             this(null, null);
         }
     }
+
+    public LinkedList() {
+        this.dummyHeader = new Node(null, null);
+        this.size = 0;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+
+    public void add(E element, int index) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("index must index<0||index>size");
+        }
+        if (index == 0) {
+            addFirst(element);
+        } else {
+            Node prev = dummyHeader;
+            for (int i = 0; i < index; i++) {
+                prev = dummyHeader.next;
+            }
+            Node node = new Node(element);
+            node.next = prev.next;
+            prev.next = node;
+            size++;
+        }
+    }
+
+    public void addFirst(E element) {
+        add(element, 0);
+    }
+
+    public void addLast(E element) {
+        add(element, size);
+    }
+
 }
