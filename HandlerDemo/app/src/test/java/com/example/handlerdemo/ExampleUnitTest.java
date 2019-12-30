3f672e19756f80e706d2066fc6649ce6122af880
tengfei <arrayadapter.cn@gmail.com>
2019-12-31 01:12:53 +0800

    study

--- HandlerDemo/app/src/test/java/com/example/handlerdemo/ExampleUnitTest.java
+++ HandlerDemo/app/src/test/java/com/example/handlerdemo/ExampleUnitTest.java
@@ -1,5 +1,9 @@
 package com.example.handlerdemo;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,8 +14,37 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+
+
+
     @Test
     public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+       new Thread(new Runnable() {
+           @Override
+           public void run() {
+               Looper.prepare();
+               Handler handler = new Handler(new Handler.Callback() {
+                   @Override
+                   public boolean handleMessage(Message msg) {
+                       System.out.println((String) msg.obj);
+                       return false;
+                   }
+               });
+               Message message01 = Message.obtain();
+               Message message02 = Message.obtain();
+               Message message03 = Message.obtain();
+               Message message04 = Message.obtain();
+               message01.obj = "1";
+               message02.obj = "2";
+               message03.obj = "3";
+               message04.obj = "4";
+               handler.sendMessage(message01);
+               handler.sendMessage(message02);
+               handler.sendMessageDelayed(message03,2000L);
+               handler.sendMessageDelayed(message04,500L);
+               Looper.loop();
+           }
+       }).start();
     }
 }
