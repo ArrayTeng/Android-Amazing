2f60f98bcdc1df7cfdbcfdde30d124b437f3e38e
tengfei <arrayadapter.cn@gmail.com>
2020-05-04 03:38:16 +0800

    手写EventBus

---
+++ libraryStudy/eventbus/src/main/java/com/amazing/tengfei/eventbus/ThreadMode.java
@@ -0,0 +1,16 @@
+package com.amazing.tengfei.eventbus;
+
+/**
+ * @author tengfei
+ * date 2020/5/4 1:17 AM
+ * email arrayadapter.cn@gmail.com
+ * description
+ */
+public enum ThreadMode {
+
+    POSTING,
+    MAIN,
+    MAIN_ORDERED,
+    BACKGROUND,
+    ASYNC
+}
