396adfd28d592d70fd1cfb2daf4b8ce2cea2e67e
滕飞 <arrayadapter.cn@gmail.com>
2021-11-08 01:05:50 +0800

    EasyJavaSsist 将Class文件加载到ClassPool中有问题，还没找到新的API

--- EasyJavaSsist/javassist/src/main/kotlin/com/tengfei/javassist/ModifyTransform.kt
+++ EasyJavaSsist/javassist/src/main/kotlin/com/tengfei/javassist/ModifyTransform.kt
@@ -4,29 +4,40 @@ import com.android.build.api.transform.Format
 import com.android.build.api.transform.QualifiedContent
 import com.android.build.api.transform.Transform
 import com.android.build.api.transform.TransformInvocation
+import com.android.build.gradle.AppExtension
 import com.android.build.gradle.internal.pipeline.TransformManager
 import com.android.utils.FileUtils
+import javassist.ClassPool
+import javassist.CtClass
 import org.gradle.api.Project
+import java.io.File
 
 class ModifyTransform(project: Project) : Transform() {
 
-    val project: Project = project
+    private val project: Project = project
+
+    private val pool = ClassPool.getDefault()
+
+
 
     override fun transform(transformInvocation: TransformInvocation?) {
         super.transform(transformInvocation)
+        print("加载的Class文件start")
+        project.extensions.findByType(AppExtension::class.java)?.bootClasspath?.forEach {
+            pool.appendClassPath(it.absolutePath)
+            print("加载的Class文件${it.absolutePath}")
+        }
         transformInvocation?.inputs?.forEach{ input ->
             // 包含我们手写的 Class 类及 R.class、BuildConfig.class 等
             input.directoryInputs.forEach { directoryInput ->
                 val path = directoryInput.file.absolutePath
-                        println("[InjectTransform] Begin to inject: $path")
 
-                // 执行注入逻辑
-                //InjectByJavassit.inject(path, mProject)
+                findTarget(directoryInput.file,path)
+
 
                 // 获取输出目录
                 val dest = transformInvocation.outputProvider.getContentLocation(directoryInput.name,
                 directoryInput.contentTypes, directoryInput.scopes, Format.DIRECTORY)
-                println("[InjectTransform] Directory output dest: $dest.absolutePath")
 
                 // 将input的目录复制到output指定目录
                 FileUtils.copyDirectory(directoryInput.file, dest)
@@ -40,6 +51,49 @@ class ModifyTransform(project: Project) : Transform() {
             }
         }
 
+    }
+
+    private fun findTarget(dir: File, fileName:String){
+        if(dir.isDirectory){
+            dir.listFiles()?.forEach {
+                findTarget(it,fileName)
+            }
+        }else{
+            val filePath = dir.absolutePath
+            if(filePath.endsWith(".class")){
+                modify(filePath,fileName)
+            }
+        }
+    }
+
+    private fun modify(filePath: String, fileName: String) {
+        if(filePath.contains("R$") || filePath.contains("R.class")
+            || filePath.contains("BuildConfig.class")){
+            return
+        }
+        val className = filePath.replace(fileName,"").replace("\\",".")
+            .replace("/",".")
+
+        val name = className.replace(".class","").substring(1)
+
+        print("class文件${name}")
+
+        val ctClass:CtClass = pool.get(name)
+
+        addCode(ctClass,fileName)
+
+
+    }
+
+    private fun addCode(ctClass: CtClass, fileName: String) {
+        ctClass.defrost()
+        val methods = ctClass.declaredMethods
+        methods.forEach {
+            it.insertBefore("if(true){}")
+        }
+
+        ctClass.writeFile(fileName)
+        ctClass.detach()
     }
 
     override fun getName(): String {
